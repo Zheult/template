cbff8a051564ad6e7dc47e5dede12f1c0611e269
Aslan Gurbanov <cppleon@gmail.com>
2016-04-20 12:44:37 +0400

    initial upload

---
+++ public/app/bootstrap.js
@@ -0,0 +1,12 @@
+/**
+ * Bootstrap angular onto the window.document node
+ */
+define([
+    'angular',
+    './app',
+    './states'
+], function (ng) {
+    'use strict';
+
+    return ng.bootstrap(document, ['app']);
+});
